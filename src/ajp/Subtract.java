4497801b16e0209685f00bd6ffb7970ecd670cee
pedrogh <pedro@Pedro-Fajardos-iMac.local>
2014-11-21 20:58:29 -0700

    New class to subtract

---
+++ src/ajp/Subtract.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ajp;
+
+/**
+ *
+ * @author pedro
+ */
+public class Subtract {
+ 
+    public Subtract() {}
+    
+    public int Sub(int a, int b) {
+        return a - b;
+    }
+}
